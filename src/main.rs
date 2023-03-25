fc6ee31d07e68edb0514b8e35804afe16c1c14c6
Michael Chovanak <mchovanak@outlook.com>
2023-03-25 16:02:55 -0700

    Set up initial chat completions pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod gpt4 {
+    pub mod api;
+    pub mod models;
+}
+
+use gpt4::api::call_gpt4_api;
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let prompt = "Translate the following English text to French: 'Hello, how are you?'";
+    let max_tokens = 50;
+
+    let result = call_gpt4_api(prompt, max_tokens).await?;
+
+    println!("Generated text: {}", result);
+
+    Ok(())
 }
